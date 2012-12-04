ff8507378dad793bff41294bd9390078b08342b1
Andrew Thorwall <andthorwall@gmail.com>
2012-12-04 01:14:39 -0500

    Added comments

--- Tree2.cpp
+++ Tree2.cpp
@@ -12,6 +12,8 @@ Tree::Tree()
 
 Tree::Tree(Grid * g)
 {
+	// create a copy of the grid to represent the state.
+	// it needs to be a copy because we will be modifying it.
 	state = new Grid(g);
 }
 
@@ -22,60 +24,74 @@ Tree::~Tree()
 
 Move * Tree::search(int team, int depth, int & val)
 {
+	// these vectors store all possible moves and their corresponding values
 	vector<int> values;
 	vector<Move*> moves;		
 	
+	// if we've reached our max depth we return the value of the state and NULL for the move
 	if (depth == SEARCH_DEPTH)
 	{
 		val = eval();
 		return NULL;
 	}
 
+	// iterate through entire board to consider every possible move
 	for (int x1 = 0; x1 < 10; x1++)
 	{
 		for (int y1 = 0; y1 < 10; y1++)
 		{	
+			// check if there's an actor at the spot
 			if (state->getActor(x1, y1))
 			{	
+				// and make sure it's on the right team
 				if (state->getActor(x1, y1)->getTeam() == team)
 				{
+					// now we check every move this piece could make,
+					// and add each move to our moves vector
+					// if it's a scout we must consider its movement
 					if (state->getActor(x1, y1)->getType() == 9)
 					{
 						for (int x2 = 0; x2 < 10; x2++)
 							if (state->isValidMove(x1, y1, x2, y1, team))
-								moves.push_back(new Move(x1, y1, x2, y1, depth));
+								moves.push_back(new Move(x1, y1, x2, y1));
 						for (int y2 = 0; y2 < 10; y2++)
 							if (state->isValidMove(x1, y1, x1, y2, team))
-								moves.push_back(new Move(x1, y1, x1, y2, depth));
+								moves.push_back(new Move(x1, y1, x1, y2));
 					}
 					else
 					{
 						if (state->isValidMove(x1, y1, x1 - 1, y1, team))
-							moves.push_back(new Move(x1, y1, x1 - 1, y1, depth));
+							moves.push_back(new Move(x1, y1, x1 - 1, y1));
 						if (state->isValidMove(x1, y1, x1 + 1, y1, team))
-							moves.push_back(new Move(x1, y1, x1 + 1, y1, depth));
+							moves.push_back(new Move(x1, y1, x1 + 1, y1));
 						if (state->isValidMove(x1, y1, x1, y1 - 1, team))
-							moves.push_back(new Move(x1, y1, x1, y1 - 1, depth));
+							moves.push_back(new Move(x1, y1, x1, y1 - 1));
 						if (state->isValidMove(x1, y1, x1, y1 + 1, team))
-							moves.push_back(new Move(x1, y1, x1, y1 + 1, depth));
+							moves.push_back(new Move(x1, y1, x1, y1 + 1));
 					}
 				}
 			}
 		}
 	}
+	// now we check which of the possible moves has the highest (or lowest, depending on which team is making the move)
+	// value
 	int bestVal = 0;
 	int bestIndex = 0;
+	// make sure there is at least one valid move
 	if (moves.size() > 0)
 	{
+		// first we check the first move and compare the rest to it
 		state->move(moves[0]->x1, moves[0]->y1, moves[0]->x2, moves[0]->y2, team);
 		search(1 - team, depth + 1, bestVal);
 		state->undoMove();
-		for (int i = 0; i < moves.size(); i++)
+		for (int i = 1; i < moves.size(); i++)
 		{
+			// alter the state by making the move
 			state->move(moves[i]->x1, moves[i]->y1, moves[i]->x2, moves[i]->y2, team);
 			int v;
+			// check the value of this state as determined by the max/min of the states that can occur after it
 			search(1 - team, depth + 1, v);
-			
+			// if it's the computer we want to maximize value
 			if (team == 0)
 			{
 				if (v > bestVal)
@@ -84,6 +100,7 @@ Move * Tree::search(int team, int depth, int & val)
 					bestVal = v;
 				}
 			}
+			// otherwise minimize it
 			else
 			{
 				if (v < bestVal)
@@ -96,6 +113,8 @@ Move * Tree::search(int team, int depth, int & val)
 		}
 								
 		val = bestVal;
+		// clean up, but if we're at a depth of 1, we're returning the move to the user
+		// so don't delete that one.
 		if (depth > 1)
 		{
 			for (int i = 0; i < moves.size(); i++) delete moves[i];
@@ -113,6 +132,8 @@ Move * Tree::search(int team, int depth, int & val)
 		return NULL;
 	}
 }
+
+// not complete at the moment
 
 int Tree::eval()
 {
--- Tree2.h
+++ Tree2.h
@@ -3,14 +3,14 @@
 
 #include "Grid.h"
 
-#define SEARCH_DEPTH 3
+#define SEARCH_DEPTH 6
 
 struct Move
 {
 	Move() { }
-	Move(int a, int b, int c, int d, int e) { x1 = a; y1 = b; x2 = c; y2 = d; depth = e; }
+	Move(int a, int b, int c, int d) { x1 = a; y1 = b; x2 = c; y2 = d; }
 
-	int x1, y1, x2, y2, depth;
+	int x1, y1, x2, y2;
 };
 
 class Tree
@@ -21,13 +21,14 @@ public:
 	Tree(Grid *);
 	~Tree();
 
-	//recursively searches the CURRENT GRID STATE for the move with the max/min value (depending on which team is going)
+	// recursively searches the CURRENT GRID STATE for the move with the max/min value (depending on which team is going)
 	Move * search(int team, int depth, int & val);
 
 	int eval();
 
 private:
 
+	// we only store one state the whole time we're searching. We alter the state as we traverse the tree.
 	Grid * state;
 
 };
